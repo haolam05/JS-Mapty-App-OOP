d30315baf6ed356a451027c1f142d8c60c4fecec
Hao Lam <tuonghao2001@gmail.com>
2023-07-01 03:14:38 -0700

    get location (Geolocation API), display map(Leftlet library), display map marker and render workout input form

--- script.js
+++ script.js
@@ -10,3 +10,57 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+let map, mapEvent;
+
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function (position) {
+    const { latitude, longitude } = position.coords;
+    const map = L.map('map').setView([latitude, longitude], 13);
+    console.log(position);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(map);
+
+    map.on(
+      'click',
+      function (e) {
+        form.classList.remove('hidden');
+        mapEvent = e;
+      },
+      function () {
+        alert('Could not find your position!');
+      }
+    );
+  });
+}
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  inputDistance.value =
+    inputDuration.value =
+    inputCadence.value =
+    inputElevation.value =
+      '';
+  const { lat: latitude, lng: longitude } = mapEvent.latlng;
+  L.marker([latitude, longitude])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('🏃‍♀️🏃‍♂️🏃‍♀️💨')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
